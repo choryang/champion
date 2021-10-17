28c969d6cb27c41967ddc901e1554415002a8890
choryang <okee0815@gmail.com>
2021-10-17 13:29:13 +0900

    수정, 저장 버튼 삭제

--- js/index.js
+++ js/index.js
@@ -27,14 +27,6 @@ $(function () {
     }
   );
 
-  $("#summernote").summernote({
-    height: 360, // 에디터 높이
-    minHeight: null, // 최소 높이
-    maxHeight: null, // 최대 높이
-    focus: true, // 에디터 로딩후 포커스를 맞출지 여부
-    lang: "ko-KR", // 한글 설정
-  });
-
   $("#search").keyup(function () {
     $(".champion").empty();
     var keyword = $("#search").val();
@@ -57,30 +49,5 @@ $(function () {
       );
     });
     printInfo();
-  });
-
-  $(".edit").click(function () {
-    $("#champion-info").css("display", "none");
-    $("#champion-info-edit").css("display", "block");
-    $("#summernote").summernote("code", $("#champion-info").html());
-  });
-
-  $(".save").click(function () {
-    var name = $("#champion-info").attr("name");
-    var result = champion.filter((champion) => champion.name === name);
-    var idx = result[0].id - 1;
-    var edited = $("#summernote").summernote("code");
-    var newInfo = {
-      id: result[0].id,
-      name: result[0].name,
-      engName: result[0].engName,
-      type: result[0].type,
-      info: edited,
-      image: result[0].image,
-    };
-    champion.splice(idx, 1, newInfo);
-    $("#champion-info").html(edited);
-    $("#champion-info").css("display", "block");
-    $("#champion-info-edit").css("display", "none");
   });
 });
